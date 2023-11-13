842cdef8e80c465618181a54f239ddade73c3770
Nfrancoo <francotobiasquiro@gmail.com>
2023-11-13 00:18:47 -0300

    SegundoParcial

---
+++ PrimerParcial/SQL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.ComponentModel.Design;
+using System.Data.SqlClient;
+
+namespace SegundoParcial
+{
+    internal class SQL
+    {
+        private SqlConnection conexion;
+        private static string cadena_conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        static SQL()
+        {
+            SQL.cadena_conexion = Properties.Resources.miConexion;
+        }
+
+        public SQL()
+        {
+            this.conexion = new SqlConnection(SQL.cadena_conexion);
+
+        }
+    }
+}
